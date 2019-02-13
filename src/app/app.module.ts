da2dd4ced432398c10854561fbcfd30cc50a6eb6
JUNIOR-PC\JUNIOR <junior9482@hotmail.com>
2019-02-13 00:07:32 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,76 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatListModule,
+  MatToolbarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import EmployeeService from './shared/api/employee.service';
+import { OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
+import { HomeComponent } from './home/home.component';
+import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: 'employee-list',
+    component: EmployeeListComponent
+  },
+  {
+    path: 'employee-add',
+    component: EmployeeEditComponent
+  },
+  {
+    path: 'employee-edit/:id',
+    component: EmployeeEditComponent
+  },
+  { path: 'implicit/callback', component: OktaCallbackComponent }
+];
+
+const config = {
+  issuer: 'https://dev-762355.oktapreview.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oajd5eg25FxUgj8T0h7'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeEditComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    AppRoutingModule,
+    OktaAuthModule.initAuth(config)
   ],
-  providers: [],
+  providers: [
+    EmployeeService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
